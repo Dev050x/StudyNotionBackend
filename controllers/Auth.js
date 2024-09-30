d83113ac22cdd0b73daa65d1e87b76ad1652d48d
divpatel4081 <divpatel4081@gmail.com>
2024-10-01 00:52:37 +0530

    sign-up controller implemented succefully

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,9 +1,11 @@
 const OTP = require("../models/OTP");
 const User = require("../models/User");
+const Profile = require("../models/Profile");
 const OtpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
 
 
-//send otp
+//send otp(generate the otp and saving into the database)
 exports.sendOTP = async (req,res) => {
     try{
         //fetching the mail
@@ -41,7 +43,7 @@ exports.sendOTP = async (req,res) => {
         console.log(otpBody);
 
         //return the responce status
-        res.status(200).json({
+        return res.status(200).json({
             success:true,
             message:"otp sent succefully",
             otp,
@@ -50,9 +52,98 @@ exports.sendOTP = async (req,res) => {
     }
     catch(error){
         console.log("there is some error in sending otp",error);
-        res.status(500).json({
+        return res.status(500).json({
             success:false,
             message: error.message,
         });
     }
+}
+
+//sign-up
+exports.signUp = async (req,res) => {
+    try{
+        //fetch the data from the request
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp,
+        } = req.body();
+        //validate the user
+        if(!email || !password || !firstName || !lastName || !confirmPassword || !otp ){
+            res.status(403).json({
+                success: false,
+                message: "All fields are required",
+            })
+        }
+
+        //password math
+        if(password != confirmPassword) {
+            res.status(400).json({
+                success:false,
+                message:"password and confirm password should be same",
+            })
+        }
+        //checking the user if already exists
+        let existingUser = await User.findOne({email});
+        if(existingUser){
+            res.status(400).json({
+                success:false,
+                message:"user already exists",
+            })
+        }
+        //finding the recent otp
+        const recentOtp = await OTP.find({email}).sort({createdAt:-1}).limit(1);  
+        console.log(recentOtp);
+        //validate the otp
+        if(recentOtp.length == 0){
+            res.status(400).json({
+                success:false,
+                message:"please enter otp properly",
+            })
+        }else if(recentOtp !== otp){
+            res.status(400).json({
+                success:false,
+                message:"please enter otp correctly",
+            })
+        }
+        //hash password
+        const hashedPassword = await bcrypt.hash(password,10);
+        //create the entry in DB
+        const profileDetails = await Profile.create({
+            gender:null,
+            dateOfBirth:null,
+            about:null,
+            contactNumber:null,
+        })
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            password:hashedPassword,
+            accountType,
+            additionalDetails: profileDetails._id,
+            image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+        })
+        //response send
+        return res.status(200).json({
+            success:true,
+            message:"user registered succefully",
+            user,
+        })
+
+    }
+    catch(error){   
+        return res.status(500).json({
+            success:false,
+            messsage:"user cannot be registered please try again",
+        })
+
+    }
+
 }
