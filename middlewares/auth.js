f7b6006981014713dd6b0bb4b1781d9f0c4de62d
divpatel4081 <divpatel4081@gmail.com>
2024-10-01 15:24:45 +0530

    student authentication middleware implemented succefully

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+con
+ 
 
 
 //auth
@@ -18,7 +20,7 @@ exports.auth = async (req,res,next) => {
 
         //verify token
         try{
-            const decode = await jwt.verify(token , process.env.JWT_SECRET);    
+            const decode =jwt.verify(token , process.env.JWT_SECRET);    
             console.log(decode);
             req.user = decode;
         }
@@ -35,5 +37,26 @@ exports.auth = async (req,res,next) => {
             success:false,
             message:"something went wrong while authenticating token",
         });
+    }
+    next();
+}
+
+//isStudent
+exports.isStudent = async (req,res,next) => {
+    try{
+        //cheking the user
+        if(req.user.accountType !== "Student"){
+            res.status(401).json({
+                success:false,
+                message:"this is protected round for student only",
+            });
+        }
+        next();
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"user role cannot be verified, please try again",
+        })
     }
 }
