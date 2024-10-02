cd1f5cbf9ce3b390759adccda95877dd145512aa
divpatel4081 <divpatel4081@gmail.com>
2024-10-02 14:43:56 +0530

    reset-Password controller implemented succefully

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -1,8 +1,9 @@
 const User = require("../models/User");
 const crypto = require("crypto");
 const mailSender = require("../utils/mailSender");
+const bcyrpt = require("bcrypt");
 
-//reset password
+//reset password token (generate the token and send the url into the mail)
 exports.resetPasswordToken = async (req,res) => {
     try{
         //fetch the mail
@@ -37,6 +38,54 @@ exports.resetPasswordToken = async (req,res) => {
         return res.status(401).json({
             success:false,
             messsage:"there is some error while sending reset-password mail",
+        })
+    }
+}
+
+//reset password (changing the mail)
+const resetPassword = async (req,res) => {  
+    try{
+        //fetching the mail
+        const {password , confirmPassword , token} = req.body();
+        //validation
+        if(password !== confirmPassword){
+            res.status(401).json({
+                success:false,
+                message:"password and confirm password mismatched",
+            })
+        }
+        //finding the user details using token
+        const userDetails = await User.findOne({token:token});
+        if(!userDetails){
+            return res.status(401).json({
+                success:false,
+                messsage:"there is no such user in database",
+            })
+        }
+        //token time cheking
+        if(userDetails.resetPasswordExpiresIn < Date.now()){
+            return res.status(403).json({
+                success:false,
+                message:"token expired",
+            })
+        }
+        //hash pwd
+        const hashedPassword = await bcyrpt.hash(password,10);
+        await User.findOneAndUpdate(
+            {token:token},
+            {password:hashedPassword},
+            {new:true},
+        );
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"password reset succefully",
+        });
+    }
+    catch(error){
+        return res.status(403).json({
+            success:false,
+            message:"reset password failed",
         })
     }
 }
